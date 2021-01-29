cd6bd06d7065897a9a78173bc9af15f6f9c87503
Lucas Mateus Menezes Silva <luacsm.99@gmail.com>
2021-01-29 00:27:22 -0300

    Projeto finalizado

---
+++ clone-discord/src/components/ChannelData/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Lucas Mateus"
+            date="21/06/2020"
+            content="Vamos assistir One Piece!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Ciclano"
+          date="21/06/2020"
+          content={
+            <>
+              <Mention>@Lucas Mateus</Mention>, bora jogar?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
